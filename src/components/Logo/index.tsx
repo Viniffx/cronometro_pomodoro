4124e4b5b996a38a21f281bea9fda33c5af74978
viniffx <viniffx@gmail.com>
2026-03-10 11:12:45 -0300

    primeiro comit

---
+++ src/components/Logo/index.tsx
@@ -0,0 +1,13 @@
+import { TimerIcon } from 'lucide-react';
+import styles from './styles.module.css';
+
+export function Logo() {
+  return (
+    <div>
+      <a href="#">
+        <TimerIcon />
+        <span>Logo</span>
+      </a>
+    </div>
+  );
+}
